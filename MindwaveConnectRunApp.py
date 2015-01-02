9fd4b4dca3113036da76ac61b41f0fd17a9a9fc5
Talamantez <robert.talamantez2@gmail.com>
2015-01-01 22:03:07 -0800

    added python script to coordinate headset connection and node app

--- MindwaveConnectRunApp.py
+++ MindwaveConnectRunApp.py
@@ -4,8 +4,6 @@ commands = [
 	'gort bluetooth unpair 20:68:9D:4C:0D:B7 hci0 ; gort bluetooth connect 20:68:9D:4C:0D:B7 hci0',    
     'sleep 10;node app.js'
 ]
-# run in parallel
+
 processes = [Popen(cmd, shell=True) for cmd in commands]
-# do other things here..
-# wait for completion
 for p in processes: p.wait()
--- runApp.py
+++
@@ -1,4 +0,0 @@
-from subprocess import call
-#call(["nvm"])
-#call(["nvm", "use", "0.10.33"])
-call(["node", "app.js"])
