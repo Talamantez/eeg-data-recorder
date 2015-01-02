83d90128e4eaf328929d5d67fa4967da991202cc
Talamantez <robert.talamantez2@gmail.com>
2015-01-02 01:04:47 -0800

    added bower_concat to concatenate front-end scripts

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+    bower_concat: {
+      all: {
+        dest: 'dist/bower.js'
+  }
+}
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-bower-concat');  
+
+  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('concat', ['bower_concat']);  
+
+};
