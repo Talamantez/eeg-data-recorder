01cb3607f8f2d7b6c3d07dbd34e13233c09b0786
Talamantez <robert.talamantez2@gmail.com>
2015-01-04 12:48:15 -0800

    fixed test for eeg.lastShot function

--- test/test.js
+++ test/test.js
@@ -1,39 +1,20 @@
 var assert = require('chai').assert;
 var eeg = require('../eegFunctions');
-var mongoose = require('mongoose');
-var db = mongoose.connection;
-var activeDB = 'eegControl'; /*use 'eeg'  for general testing', 
-                        'eegNap'   for napping,
-                        'eegSleep' for sleeping,
-                        'eegAwake' for wake recording
-                        Be sure this matches db in models/eegSnapshot
-                        and to switch mongo  to this db,
-                        'eegControl' for control data                          
-                        and 'eegMock' for mock data
-                        */ 
-var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/' + activeDB;
-var newData;
-var avgData;
-var avg1000Data;
 
-mongoose.connect(uristring);
-		    db.on('error', console.error);
-		    db.once('open',function callback(){
-		    	console.log('db '+ activeDB + ' ready');
-		});	
 
 suite("eeg", function(){
 
 	test("lastShot should return an array", function(){
-		
-		eeg.lastShot(function(data){
+		console.log('hi');
+		eeg.lastShot(function(data,callback){
 			newData = data;
 			console.log('newData: ' + newData);
+			callback(testit);
 		});
-
-		return assert.typeOf(newData 
+		var testit = function(){
+			return assert.typeOf(newData 
 			,'array'
-			, 'eeg.lastShot() returns an array');
+			, 'eeg.lastShot() returns an array'
+			)};
 	});	
 });
-
