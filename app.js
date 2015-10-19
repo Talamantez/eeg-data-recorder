eca23575b56e2791fbae2ce3c8e51c2d06ce910e
Robert Talamantez <robert.m.talamantez@gmail.com>
2015-10-18 22:02:45 -0700

    setting up mongo

--- app.js
+++ app.js
@@ -1,38 +1,52 @@
 'use strict'
 
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var cylon = require('cylon');
-var mongoose = require('mongoose');
-var io = require('socket.io')(http);
-var eeg = require('./eegFunctions');
+var express     = require( 'express' ),
+    app         = express(),
+    http        = require( 'http' ).Server( app ),
+    cylon       = require( 'cylon' ),
+    mongoose    = require( 'mongoose' ),
+    io          = require( 'socket.io' )( http ),
+    eeg         = require( './eegFunctions' );
 
 var port = process.env.PORT || 3000 ;
 
-app.use(express.static(__dirname + '/dist'));
+var db = mongoose.connection;
 
-var headsetPort = '/dev/rfcomm0'; //this might be rfcomm0, rfcomm1, or rfcomm2. Check your connection.
-var activeDB = 'eegControl'; /*use 'eeg'  for general testing', 
-                        'eegNap'   for napping,
-                        'eegSleep' for sleeping,
-                        'eegAwake' for wake recording
-                        Be sure this matches db in models/eegSnapshot
-                        and to switch mongo  to this db,
-                        'eegControl' for control data                          
-                        and 'eegMock' for mock data
+var headsetPort = '/dev/rfcomm0'; 
+                        /* 
+                            this might be rfcomm0, 
+                            rfcomm1, 
+                            or rfcomm2. 
+                            Check your connection.
+                        */
+
+var activeDB    = 'eegControl'; 
+                        /*
+                            use 'eeg'  for general testing', 
+                            'eegNap'   for napping,
+                            'eegSleep' for sleeping,
+                            'eegAwake' for wake recording
+                            Be sure this matches db in models/eegSnapshot
+                            and to switch mongo  to this db,
+                            'eegControl' for control data                          
+                            and 'eegMock' for mock data
                         */ 
 
 var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/' + activeDB;
 
-var db = mongoose.connection;
-db.on('error', console.error);
-db.once('open',function callback(){
-        console.log('db '+ activeDB + ' ready');
+app.use( express.static( __dirname + '/dist' ) );
+
+
+
+
+
+db.on( 'error' , console.error );
+db.once( 'open', function callback(){
+        console.log( 'db ' + activeDB + ' ready' );
 });
 
-http.listen(port, function(){
-    console.log('\nListening on port:' + port + '\n');
+http.listen( port, function(){
+    console.log( '\nListening on port:' + port + '\n' );
 });
 
 /*
@@ -41,7 +55,7 @@ Log out a message when a user connects to the website
 
 */
 
-io.on('connection', function(socket){
+io.on( 'connection' , function(socket){
     console.log('a user connected');
 });
 
@@ -51,7 +65,8 @@ Connect to MongoDB
 
 */
 
-mongoose.connect(uristring);
+mongoose.connect( uristring );
+
 /*
 
     Initialize the Cylon robot,
@@ -61,37 +76,35 @@ mongoose.connect(uristring);
 */
 
 cylon.robot({
-  connection: { name: 'neurosky', adaptor: 'neurosky', port: headsetPort },
-  device: { name: 'headset', driver: 'neurosky' }
+      connection:   { name: 'neurosky', adaptor: 'neurosky', port: headsetPort },
+      device:       { name: 'headset', driver: 'neurosky' }
 })
-.on('ready', function(robot) {
-  var newData;
-  var avg10Data;
-  var avg1000Data;  
-  robot.headset.on('eeg', function(data) {
-    eeg.addShot(	data.delta,
-        			data.theta,
-        			data.loAlpha,
-        			data.hiAlpha,
-        			data.loBeta,
-                    data.hiBeta,
-        			data.loGamma,
-        			data.midGamma
-    		),
-
-    eeg.lastShot(function(data){
-        newData = data;
-    }),
-
-    eeg.avgLastTen(function(data){
-        avg10Data = data;
-    }),
-    eeg.avgLast1000(function(data){
-        avg1000Data = data;
-    }),
-    sendData(newData,avg10Data,avg1000Data);
-
-})})
+.on( 'ready' , function(robot) {
+    var newData;
+    var avg10Data;
+    var avg1000Data;  
+    robot.headset.on( 'eeg' , function(data) {
+        eeg.addShot(	data.delta,
+            			data.theta,
+            			data.loAlpha,
+            			data.hiAlpha,
+            			data.loBeta,
+                        data.hiBeta,
+            			data.loGamma,
+            			data.midGamma
+        		),
+        eeg.lastShot(function(data){
+            newData = data;
+        }),
+        eeg.avgLastTen(function(data){
+            avg10Data = data;
+        }),
+        eeg.avgLast1000(function(data){
+            avg1000Data = data;
+        }),
+        sendData(newData,avg10Data,avg1000Data);
+    })
+})
 .start();
 
 /* 
@@ -102,20 +115,20 @@ Helper Functions
 
 // Send data to website using socket.io
 function sendData(newData,avg10,avg1000){
-    var brainDataChunk = [newData,avg10,avg1000];
-    console.log(brainDataChunk);
-    io.sockets.emit('brain-data', brainDataChunk);
+    var brainDataChunk = [ newData , avg10 , avg1000 ];
+    console.log( brainDataChunk );
+    io.sockets.emit( 'brain-data' , brainDataChunk );
 }
 
 // Send simulated brain data using socket.io
 function mockHeadset(){
-    var mockNew = eeg.mockBrainData();
-    var mock10Avg = eeg.mockBrainData();
-    var mock1000Avg = eeg.mockBrainData();
+    var mockNew                 = eeg.mockBrainData();
+    var mock10Avg               = eeg.mockBrainData();
+    var mock1000Avg             = eeg.mockBrainData();
 
-    console.log('mockNew' + mockNew);
-    console.log('mock10Avg' + mock10Avg);
-    console.log('mock1000Avg' + mock1000Avg);
+    console.log('mockNew'       + mockNew);
+    console.log('mock10Avg'     + mock10Avg);
+    console.log('mock1000Avg'   + mock1000Avg);
 
     sendData(mockNew,mock10Avg,mock1000Avg);
 }
