17706abd9cf5e9f059b893025acb25b5b7fe3b6d
Robert Talamantez <robert.m.talamantez@gmail.com>
2015-10-18 20:47:42 -0700

    update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,26 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON( 'package.json' ),
+    copy: {
+        main:{
+            files:[
+                {
+                  expand: true, 
+                  src: ['src/html/*.html'], 
+                  dest: 'dist/',
+                  flatten: true
+                },
+                {
+                  expand: true, 
+                  src: ['src/assets/fonts/*'], 
+                  dest: 'dist/fonts/', 
+                  filter: 'isFile', 
+                  flatten: true
+                }
+            ]
+        }
+    },    
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
       options: {
@@ -13,10 +33,17 @@ module.exports = function(grunt) {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
     },
-    bower_concat: {
-        all: {
-          dest: 'src/js/bower.js'
-      }
+    concat: {
+        options: {
+            // define a string to put between each file in the concatenated output
+            separator: ';'
+        },
+        dist: {
+            // the files to concatenate
+            src: ['bower_components/jquery/dist/jquery.js', 'bower_components/bootstrap/dist/js/bootstrap.js', 'bower_components/bootstrap-select/dist/js/bootstrap-select.js'],
+            // the location of the resulting JS file
+            dest: 'dist/js/<%= pkg.name %>.js'
+        }
     },
     uglify: {
        bower: {
@@ -25,35 +52,37 @@ module.exports = function(grunt) {
           compress: true
         },
           files: {
-            'dist/js/bower.min.js': 'src/js/bower.js'
+            'dist/js/<%= pkg.name %>.js': 'dist/js/<%= pkg.name %>.js'
           } 
       }
     },
     cssmin: {
-      target: {
-        files: [{
-          expand: true,
-          cwd: 'src/css',
-          src: ['*.css', '!*.min.css'],
-          dest: 'dist/css',
-          ext: '.min.css'
-        }]
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      combine: {
+        files: {
+          'dist/css/<%= pkg.name %>.css': ['bower_components/bootstrap/dist/css/bootstrap.css','bower_components/bootstrap-select/dist/css/bootstrap-select.css','src/css/*.css']
+        }
       }
-   
-  }
-    
+    },
+    clean: [ 'dist' ]
   });
   require('load-grunt-tasks')(grunt);
 
-  grunt.registerTask('default', ['jshint', 'buildbower','buildcss','test']);
-  grunt.registerTask('concat', ['bower_concat']);
-  grunt.registerTask('buildbower', [
-    'bower_concat',
-    'uglify:bower'
-    ]);
-  grunt.registerTask('buildcss',[
-    'cssmin'
-    ]);
+  grunt.registerTask('cleanit', ['clean']);
+
+  grunt.registerTask('cssit',['cssmin']);
+
+  grunt.registerTask('concatit', ['concat']);
+
+  grunt.registerTask('uglyit', ['uglify']);
+  
+  grunt.registerTask('hintit', ['jshint']);
+
+  grunt.registerTask('copyit', ['copy']);
+
   grunt.registerTask('test', 'run tests', function () {
     var done = this.async();
     require('child_process').exec('npm test', function (err, stdout) {
@@ -61,5 +90,8 @@ module.exports = function(grunt) {
       done(err);
     });
   });
+
+  grunt.registerTask('notest', ['cleanit', 'cssit', 'concatit', 'uglyit', 'copyit']);
+  grunt.registerTask('default', ['cssit', 'concatit', 'uglyit', 'hintit', 'test']);
 
 };
--- connect-and-run.py
+++ connect-and-run.py
@@ -1,7 +1,7 @@
 from subprocess import Popen
 
 commands = [
-	'gort bluetooth unpair 20:68:9D:4C:0D:B7 hci0 ; gort bluetooth connect 20:68:9D:4C:0D:B7 hci0',    
+	'gort bluetooth unpair 20:68:9D:4C:0D:B7 hci1 ; gort bluetooth connect 20:68:9D:4C:0D:B7 hci1',    
     'sleep 14;pm2 start app.js'
 ]
 
