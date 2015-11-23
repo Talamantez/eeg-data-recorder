779c618f944844c4672093b5936426debbeecc52
Robert Talamantez <robert.m.talamantez@gmail.com>
2015-11-22 19:50:03 -0800

    edit run script

--- connect-and-run.py
+++ connect-and-run.py
@@ -1,8 +1,10 @@
 from subprocess import Popen
 
 commands = [
-	'gort bluetooth pair 20:68:9D:4C:0D:B7','gort bluetooth connect 20:68:9D:4C:0D:B7',    
-    'sleep 14;node app.js'
+	'bluetooth-agent 0000',
+	'sudo rfcomm connect rfcomm0 20:68:9D:4C:0D:B7'
+	'sleep 5;gort bluetooth pair 20:68:9D:4C:0D:B7','gort bluetooth connect 20:68:9D:4C:0D:B7',    
+    'sleep 10;node app.js'
 ]
 
 processes = [Popen(cmd, shell=True) for cmd in commands]
